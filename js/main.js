c6308e028efdf05b7a2820b58ff5da17a963bcf0
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-11 13:57:40 +0200

    build: add base project files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*Data una lista della spesa, stampare sulla pagina (anche brutalmente, 
+basta che si vedano) gli elementi della lista individualmente con un ciclo while*/
+
+//creare un array che elenca gli elementi della spesa
+
+//dichiarare la variabile da associare al singolo elemento in lista
+
+//estrarre tutti i valori della lista array
+
+//selezionare la lista
+
+//creare elementi lista e stamparli
