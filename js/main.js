fba4427210777f7622cfca623596338a36f302e4
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-11 16:20:14 +0200

    feat: add js code to extract and insert list in html web page

--- js/main.js
+++ js/main.js
@@ -2,11 +2,43 @@
 basta che si vedano) gli elementi della lista individualmente con un ciclo while*/
 
 //creare un array che elenca gli elementi della spesa
+const myList = [
+    "patate", 
+    "pomodori", 
+    "olio d'oliva", 
+    "vino",
+    "pasta", 
+    "gamberi",
+    "broccoli",
+    "farina",
+    "uova",
+    "caffè",    
+    "biscotti",
+    "frutta",
+    "gelato"
+];
+
+console.log(myList);
 
 //dichiarare la variabile da associare al singolo elemento in lista
+let i = 0;
 
-//estrarre tutti i valori della lista array
+//selezionare l'd che corrisponde alla lista ul in HTML
+let divList = document.querySelector("#list");
 
-//selezionare la lista
+while (i < myList.length) {
 
-//creare elementi lista e stamparli
+    //estrarre tutti i valori della lista array per tutta la lunghezza dell'array
+    let contentList = myList[i];
+    console.log(contentList);
+
+    //creare elementi lista e stamparli
+    divList.innerHTML += `<li>${contentList}</li>`;
+
+    //incrementare il singolo elemento della lista
+    i++;
+
+}
+
+
+
